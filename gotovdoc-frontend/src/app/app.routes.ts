d9c106867ed25ba789e1a03c00d58c36b394122b
Simeon Spasov <simeon.spasov@ipi-soft.com>
2025-08-04 21:27:19 +0300

    Initial commit

---
+++ gotovdoc-frontend/src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: '', pathMatch: 'full', redirectTo: 'home' },
+      {
+        path: 'home',
+        loadComponent: () => import('./features/home/home').then((m) => m.Home),
+      },
+      {
+        path: 'about',
+        loadComponent: () => import('./features/about/about').then((m) => m.About),
+      },
+    ],
+  },
+  { path: '**', redirectTo: '' },
+];
